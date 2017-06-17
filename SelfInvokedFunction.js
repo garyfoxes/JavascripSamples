012cd61bcae0051944e2463b0bb83daa8c7fbfd8
garyfox <gary@amazing.com>
2017-06-17 22:14:56 +0100

    Self Invoked Function

---
+++ SelfInvokedFunction.js
@@ -0,0 +1,14 @@
+/**
+ * Created by garyfox on 17/06/2017.
+ */
+'use strict';
+//Assigns value straight away to variable let
+let result = function (num1,num2) {
+    return num1 * num2;
+}(2,5);
+console.log(result);
+
+//invoke anonymous function
+(function (num1,num2) {
+    console.log(num1 * num2);
+})(3,4);
